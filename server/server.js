4d334c62af83654a1aae4f4b51827ac3dbe93825
Dinesh Kumarcc <dinesh.codecorners@gmail.com>
2021-12-15 10:21:07 +0530

    check

--- server/server.js
+++ server/server.js
@@ -48,7 +48,7 @@ app.prepare().then(async () => {
     const shop = ctx.request.query.shop
 
     const shopData = [];
-    const shopId='';
+    const shopId ='';
     const notificationsData = [];
     const shopsRef = collection(db, "shop");  
     const q = query(shopsRef, where("shop", "==", shop), limit(1));
@@ -56,7 +56,7 @@ app.prepare().then(async () => {
     console.log(querySnapshot,'quersnap')
     querySnapshot.forEach(async(d)=>{
       const data = d.data();
-      shopId(data.id)
+      shopId.push(data.id)
       console.log(shopId,'shopid----')
       if(shop === data.shop){
         shopData.push({...data, id: d.id});
